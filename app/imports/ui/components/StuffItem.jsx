3ea62d757ce405e2cbc9e34b89798ac575bf6bad
klin6 <link6@hawaii.edu>
2018-10-30 11:22:47 -1000

    delete button complete

--- app/imports/ui/components/StuffItem.jsx
+++ app/imports/ui/components/StuffItem.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import { Table } from 'semantic-ui-react';
+import { Table, Button } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
+import { Bert } from 'meteor/themeteorchef:bert';
+import { Stuffs } from '/imports/api/stuff/stuff';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class StuffItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+  onClick() {
+    Stuffs.remove(this.props.stuff._id, this.deleteCallback);
+  }
+
+  deleteCallback(error) {
+    if (error) {
+      Bert.alert({ type: 'danger', message: `Add failed: ${error.message}` });
+    } else {
+      Bert.alert({ type: 'success', message: 'Add succeeded' });
+    }
+  }
+
   render() {
     return (
         <Table.Row>
@@ -13,6 +31,9 @@ class StuffItem extends React.Component {
           <Table.Cell>{this.props.stuff.condition}</Table.Cell>
           <Table.Cell>
             <Link to={`/edit/${this.props.stuff._id}`}>Edit</Link>
+          </Table.Cell>
+          <Table.Cell>
+            <Button onClick={this.onClick}>Delete</Button>
           </Table.Cell>
         </Table.Row>
     );
